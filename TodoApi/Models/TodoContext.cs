1f733493639de44051950db1fcb045226051cbdf
430009048 <430009048@H41780LAPLGP>
2024-03-16 13:23:03 -0600

    Agregar archivos de proyecto.

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Controllers.Models;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) 
+        {
+        
+        }
+        public DbSet<TodoItem> todoItems { get; set; } = null!;
+
+    }
+}
